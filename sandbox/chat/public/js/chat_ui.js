46574889b8f4180ac2feff6d3410405a84265863
Peter Suggitt <me@suggs.org.uk>
2015-09-28 16:07:53 +0100

    hgjg

---
+++ sandbox/chat/public/js/chat_ui.js
@@ -0,0 +1,45 @@
+function divEscapedContentElement(message) {
+    return $('<div></div>').text(message);
+}
+
+
+function divSystemContentElement(message) {
+    return $('<div></div>').html('<i>' + message + '</i>');
+}
+
+function processUserInput(chatApp, socket) {
+    var message = $('#send-message').val();
+    var systemMessage;
+
+    if (message.charAt(0) == '/') {
+        systemMessage = chatApp.processCommand(message);
+        if (systemMessage) {
+            $('#messages').append(divSystemContentElement(systemMessage));
+        }
+    } else {
+        chatApp.sendMessage($('#room').text(), message);
+        $('#messages').append(divEscapedContentElement(message));
+        $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+    }
+
+    $('#send-message').val('');
+}
+
+var socket = io.connect();
+
+$(document).ready(function () {
+    var chatApp = new Chat(socket);
+    socket.on('nameResult', function (result) {
+        var message;
+        if (result.success) {
+            message = 'You are now known as ' + result.name + '.';
+        }
+        else {
+            message = result.message;
+        }
+        $('#messages').append(divSystemContentElement(message));
+    });
+
+    socket.on('joinResult', function(result){});
+});
+
